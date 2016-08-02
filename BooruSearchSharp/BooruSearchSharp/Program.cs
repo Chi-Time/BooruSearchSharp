671bc7886b2f170faa776eeb2dd9e5ec52c6c62f
[Guardevoir] <music1384@yahoo.co.uk>
2016-08-02 12:10:10 +0100

    Initial Stable Build. No program logic yet. Just template program ready for development.

---
+++ BooruSearchSharp/BooruSearchSharp/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.IO;
+
+
+namespace BooruSearchSharp
+{
+    class Program
+    {
+        public static void Main (string[] args) => new Program ().Start ();
+
+        void Start ()
+        {
+            Console.WriteLine ("Program Started\n\n");
+
+            Console.ReadKey ();
+        }
+    }
+}
