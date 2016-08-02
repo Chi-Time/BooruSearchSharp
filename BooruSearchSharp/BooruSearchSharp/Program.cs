3f694c057cb1d4d8355b0d6b75e385215a04d1f9
[Guardevoir] <music1384@yahoo.co.uk>
2016-08-02 12:14:37 +0100

    Added updated dev program.

--- BooruSearchSharp/BooruSearchSharp/Program.cs
+++ BooruSearchSharp/BooruSearchSharp/Program.cs
@@ -1,6 +1,10 @@
 ﻿using System;
 using System.IO;
-
+using System.Collections.Generic;
+using Newtonsoft.Json;
+using System.Xml;
+using System.Net;
+using System.Xml.Serialization;
 
 namespace BooruSearchSharp
 {
@@ -10,9 +14,39 @@ namespace BooruSearchSharp
 
         void Start ()
         {
-            Console.WriteLine ("Program Started\n\n");
+            Console.WriteLine ("Program Entered.\n\n\n");
+            //var sb = new SearchBooru.SearchBooru ();
+            //var db = new DanBooru.DanBooru ();
+            //var yd = new Yandere.Yandere ();
+            //var kc = new Konachan.Konachan ();
+            var lb = new Lolibooru.Lolibooru ();
 
             Console.ReadKey ();
+        }
+
+        string Shit ()
+        {
+            // Create a request for the URL. 
+            WebRequest request = WebRequest.Create (
+              "http://gelbooru.com/index.php?page=dapi&s=post&q=index&tags=vore");
+            // If required by the server, set the credentials.
+            request.Credentials = CredentialCache.DefaultCredentials;
+            // Get the response.
+            WebResponse response = request.GetResponse ();
+            // Display the status.
+            Console.WriteLine (( (HttpWebResponse)response ).StatusDescription);
+            // Get the stream containing content returned by the server.
+            Stream dataStream = response.GetResponseStream ();
+            // Open the stream using a StreamReader for easy access.
+            StreamReader reader = new StreamReader (dataStream);
+            // Read the content.
+            string responseFromServer = reader.ReadToEnd ();
+            // Display the content.
+            //Console.WriteLine (responseFromServer);
+            // Clean up the streams and the response.
+            reader.Close ();
+            response.Close ();
+            return responseFromServer;
         }
     }
 }
